37b25d15c7c46ee236162bec4adb286db6f933a5
Jana-F <jana@fedra.cz>
2017-07-07 14:59:25 +0200

    Update project.py

--- project.py
+++ project.py
@@ -1,7 +1,8 @@
 from graphs import render_graph
-from twitter import download_followers, count_followers
+from twitter import download_followers, count_followers, download_tweets
 
 if __name__ == '__main__':
-    #download_followers('yedpodtrzitko')
-    followers_week = count_followers('yedpodtrzitko', '2017-06-30', '2017-07-05')
-    render_graph(followers_week)
+    # download_followers('yedpodtrzitko')
+    followers_date_and_sum = count_followers('yedpodtrzitko', '2017-06-30', '2017-07-06')
+    download_tweets('yedpodtrzitko')
+    render_graph(followers_date_and_sum)
