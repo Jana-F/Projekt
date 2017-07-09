0140b41e0e52ccce110ad31253a7a4886101196e
Jana Fedrová <jana@fedra.cz>
2017-07-09 19:31:41 +0200

    new variable graph_tweets (the count_tweets function)

--- project.py
+++ project.py
@@ -1,14 +1,15 @@
 from datetime import datetime
 
 from graphs import render_graph
-from twitter import download_followers, count_followers, download_tweets, get_user
+from twitter import download_followers, count_followers, download_tweets, get_user, count_tweets
 
 if __name__ == '__main__':
     user = get_user('yedpodtrzitko')
     since = datetime(2017, 6, 30)
     til = datetime(2017, 7, 6)
-    #download_followers(user)
+    # download_followers(user)
     graph_data = count_followers(user, since, til)
     graph_data['user'] = user
     download_tweets(user)
-    render_graph(graph_data)
+    graph_tweets = count_tweets(user, since, til)
+    render_graph(graph_data, graph_tweets)
